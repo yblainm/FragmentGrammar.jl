e23ad9745cab3df24dda9701a0f9a2f6498c7697
yblainm <yves.montesano@hotmail.com>
2019-03-05 15:29:44 -0500

    Work on main struct, holding pseudocounts, bookkeeping. Start with DM.

--- src/FragmentGrammars.jl
+++ src/FragmentGrammars.jl
@@ -1,7 +1,32 @@
 module FragmentGrammars
 
-using .parse_a_tree
+using GeneralizedChartParsing
+include("CompoundDists.jl"); using .CompoundDists
+include("parse_a_tree.jl")
 
+mutable struct FragmentGrammar{C}
+    baseGrammar :: Grammar
+    restaurants :: Dict{C,ChineseRest}
 
+    # For these, index by rule index or by the rule function itself?
+    pi :: Dict{Tuple{String, Int}, Int}
+    psi :: Dict{Int}
+end
+
+function FragmentGrammar(g :: Grammar)
+
+end
+
+# println(FragmentGrammar(g))
+# println("test")
+
+# println(g.categories)
+
+str = "the dog paints prep dog"
+scores = parse(g, split(str))
+g2 = add_score(g, :enum_forest, enum_forest_score)
+score = parse(g2, split(str))
+
+println(scores)
 
 end
